4564a0e344ac77641280785fdb3f6e0230ae364d
Goongam <dlwjddn341@naver.com>
2023-07-03 17:03:44 +0900

    Feat: 업로드 에러처리

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -12,8 +12,9 @@ import {
   ChangeEvent,
   MouseEvent,
 } from "react";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import FilesIcon from "./ui/icons/FileIcon";
+import Loading from "./ui/Loading";
 
 interface Props {
   groupId: string;
@@ -36,6 +37,26 @@ export default function NewPost({ groupId }: Props) {
   const [dragging, setDragging] = useState(false);
   const [images, setImages] = useState<File[]>([]);
   const commentRef = useRef<HTMLTextAreaElement>(null);
+
+  const {
+    isLoading: mutating,
+    mutate,
+    isError: mutateError,
+  } = useMutation(
+    (formdata: FormData) =>
+      fetch(`/api/post/new`, {
+        method: "post",
+        body: formdata,
+      }).then((res) => res.json()),
+    {
+      onSuccess() {
+        router.push(`/group/${groupId}`);
+      },
+      onError() {
+        console.log("에러처리");
+      },
+    }
+  );
 
   const router = useRouter();
   // const imgArray = Array.from(images ?? []);
@@ -95,12 +116,8 @@ export default function NewPost({ groupId }: Props) {
     formdata.append("group", groupId);
 
     //TODO: 에러처리, 전송 로딩
-    fetch(`/api/post/new`, {
-      method: "post",
-      body: formdata,
-    }).then(() => {
-      router.push(`/group/${groupId}`);
-    });
+
+    mutate(formdata);
   };
 
   const handleImgDelete = (index: number) => {
@@ -113,12 +130,22 @@ export default function NewPost({ groupId }: Props) {
   };
   return (
     <section className="w-full flex-col items-center">
+      {mutating && (
+        <div className="w-full h-full absolute bg-slate-300/75 flex justify-center items-center">
+          <Loading />
+        </div>
+      )}
       <h2 className="border-b-4 border-gray-400 p-4 pb-8 mx-2 mb-10 font-bold text-2xl">
         {group?.name} · 새 글 작성
       </h2>
+      {mutateError && (
+        <div className="w-full h-32 bg-red-300 flex justify-center items-center p-5 text-white text-2xl font-bold mb-1">
+          업로드 중 오류가 발생하였습니다. 다시 시도해주세요
+        </div>
+      )}
       <form
         onSubmit={handleSubmit}
-        className="w-full flex flex-col items-center px-4"
+        className="w-full flex flex-col items-center px-4 max-w-3xl mx-auto"
       >
         <input
           type="file"
@@ -129,7 +156,7 @@ export default function NewPost({ groupId }: Props) {
         />
         <label
           htmlFor="input-form"
-          className="flex justify-center items-center w-full h-52 bg-slate-100 max-w-3xl"
+          className="flex justify-center items-center w-full h-52 bg-slate-100"
           onDragEnter={handleDrag}
           onDragLeave={handleDrag}
           onDragOver={handleDragOver}
@@ -137,7 +164,7 @@ export default function NewPost({ groupId }: Props) {
         >
           <FilesIcon />
         </label>
-        <div className="w-full grid grid-cols-5 max-w-3xl">
+        <div className="w-full grid grid-cols-5">
           {getImageElement(images).map((img, index) =>
             img ? (
               <div
@@ -166,12 +193,12 @@ export default function NewPost({ groupId }: Props) {
           )}
         </div>
         <textarea
-          className="w-full h-52 max-w-3xl mt-4 border border-black rounded-md"
+          className="w-full h-52 mt-4 border border-black rounded-md"
           ref={commentRef}
           placeholder="코멘트 작성..."
           required
         />
-        <button className="w-full h-10 max-w-3xl bg-green-400 text-white font-bold mt-4 rounded-lg">
+        <button className="w-full h-10 bg-green-400 text-white font-bold mt-4 rounded-lg">
           업로드
         </button>
       </form>
