276c7cbf5f58ecae37876cfc3ea446271c5b5ec1
Goongam <dlwjddn341@naver.com>
2023-12-26 21:08:48 +0900

    feat: 이미지 최적화

--- src/components/PostImages.tsx
+++ src/components/PostImages.tsx
@@ -1,4 +1,5 @@
 import Carousel from "better-react-carousel";
+import Image from "next/image";
 
 export default function PostImages({ images }: { images: string[] }) {
   const isOneImage = images.length <= 1;
@@ -15,11 +16,22 @@ export default function PostImages({ images }: { images: string[] }) {
     >
       {images.map((image, idx) => (
         <Carousel.Item key={`${image}${idx}`}>
-          <img
-            className="w-full object-contain aspect-square rounded-sm"
-            // width="100%"
-            src={image}
-          />
+          {/* TODO: 이미지 호스팅 상수 배열 만들기 */}
+          {new URL(image).hostname === "i.imgur.com" ? (
+            <Image
+              className="w-full object-contain aspect-square rounded-sm"
+              src={image}
+              width={1000}
+              height={1000}
+              alt="Upload post image"
+            />
+          ) : (
+            <img
+              className="w-full object-contain aspect-square rounded-sm"
+              // width="100%"
+              src={image}
+            />
+          )}
         </Carousel.Item>
       ))}
     </Carousel>
